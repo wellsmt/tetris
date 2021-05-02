7b3b1fe02223ab0489574bf6f912c076830c94db
Michael T. Wells <michaelthomaswells@gmail.com>
2021-05-01 22:46:38 -0600

    line clearing
    Signed-off-by: Michael T. Wells <michaelthomaswells@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ncurses.h>
 #include <thread>
+#include <vector>
 #include "win.h"
 #include "create_box.h"
 
@@ -103,11 +104,18 @@ int main()
     int nCurrentY = 0;
     int ch = 0;
     bool bRotateHold = false;
+    int nSpeed = 20;
+    int nSpeedCounter = 0;
+    bool bForceDown = false;
+
+    std::vector<int> vLines;
 
     while (!bGameOver)
     {
         // GAME TIMING
         std::this_thread::sleep_for(50ms);
+        nSpeedCounter++;
+        bForceDown = (nSpeedCounter == nSpeed);
         // INPUT
         if (kbhit())
         {
@@ -134,6 +142,56 @@ int main()
         {
             bRotateHold = false;
         }
+
+        if(bForceDown)
+        {
+            if(DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
+            {
+                nCurrentY++;
+            } else {
+                // Lock the current piece in the field
+                for(int px = 0;px < 4; px++)
+                {
+                    for(int py = 0;py < 4;py++)
+                    {
+                        if(tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'X')
+                            pField[(nCurrentY + py) * nFieldWidth + (nCurrentX + px)] = nCurrentPiece + 1;
+                    }
+                }
+
+                for(int py=0;py < 4;py++)
+                {
+                    if(nCurrentY + py < nFieldHeight - 1)
+                    {
+                        bool bLine = true;
+                        for(int px = 1;px < nFieldWidth-1;px++)
+                        {
+                            bLine &= (pField[(nCurrentY + py) * nFieldWidth + px]) != 0;
+                        }
+
+                        if(bLine)
+                        {
+                            for(int px = 1;px < nFieldWidth - 1;px++)
+                                pField[(nCurrentY + py) * nFieldWidth + px] = 8;
+
+                            vLines.push_back(nCurrentY + py);
+                        }
+                    }
+                }
+
+                // have we created any full horizontal lines
+
+                // choose the next piece
+                nCurrentX = nFieldWidth / 2;
+                nCurrentY = 0;
+                nCurrentRotation = 0;
+                nCurrentPiece = rand() % 7;
+
+                // if piece does not fit end game
+                bGameOver = !DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY);
+            }
+            nSpeedCounter = 0;
+        }
         // RENDER OUTPUT
 
         DrawField(pField);
@@ -146,6 +204,25 @@ int main()
                 if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'X')
                     mvaddch(py + 2 + nCurrentY, px + 2 + nCurrentX, nCurrentPiece + 65);
             }
+        }
+
+        if(!vLines.empty())
+        {
+            DrawField(pField);
+            std::this_thread::sleep_for(400ms);
+
+            for(auto &v : vLines)
+            {
+                for(int px = 1;px < nFieldWidth - 1;px++)
+                {
+                    for(int py = v;py > 0;py--)
+                    {
+                        pField[py * nFieldWidth + px] = pField[(py - 1) * nFieldWidth + px];
+                    }
+                    pField[px] = 0;
+                }
+            }
+            vLines.clear();
         }
 
         refresh();
